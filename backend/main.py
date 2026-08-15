b8b09927a76912ffc550fa1dc65b0a63c1005873
Sureboy25 <slymanmatogolo22@gmail.com>
2026-08-15 19:17:15 +0300

    Allow GitHub Pages frontend in CORS

--- backend/main.py
+++ backend/main.py
@@ -36,9 +36,9 @@ app.mount(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-    ],
+    "http://localhost:3000",
+    "https://sureboy25.github.io",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
